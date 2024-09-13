cea86b144ef90c63a191de43a651786b3bccdf6b
venkat-yalamanchili <venkatyalamanchili56@gmail.com>
2024-09-12 21:26:30 -0700

    added commets to describe the code

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,17 @@
+# Use the official Python 3.9 image from the Docker Hub
 FROM python:3.9
+
+# Copy the current directory contents into the /app directory in the container
 COPY . /app
+
+# Set the working directory to /app
 WORKDIR /app
+
+# Install any required packages specified in requirements.txt
 RUN pip install -r requirements.txt
+
+# Set the entrypoint to run Python when the container starts
 ENTRYPOINT ["python"]
+
+# Specify the default command to run the app (app.py)
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-#import streamlit as st
-
-#st.title('Hello World - Team 4')
+# Import the Flask class from the flask module
 from flask import Flask
 
+# Create an instance of the Flask class for the web application
 app = Flask(__name__)
 
+# Define a route for the root URL ('/')
 @app.route('/')
 def index():
-    return '<h2 align = "center">Hello team 4</h2>'
+    # Return a simple HTML response with a message aligned to the center
+    return '<h2 align="center">Hello team 4</h2>'
 
-
+# Run the application
 if __name__ == "__main__":
+    # Start the Flask application on host '0.0.0.0' and port '5004' with debug mode enabled
     app.run(host="0.0.0.0", port="5004", debug=True)
-
 
 
 
