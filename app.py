80ffdba231b754cd15555165c4c0079288803bad
Vaishnaviuppari37 <008448837@coyote.csusb.edu>
2024-09-13 01:29:47 -0700

    app.py updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def main():
     st.title("Hello from Team 4")
     
 if __name__ == "__main__":
-    # If not already running in a Streamlit subprocess, start one
+    # If not already running in a Streamlit subprocess,  start one
     if not os.getenv('STREAMLIT_RUNNING'):
         os.environ['STREAMLIT_RUNNING'] = '1'
         subprocess.run(["streamlit", "run", __file__, "--server.port=5004"])
