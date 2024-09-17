9b49571629714c989a5c18de32b4c9ca5b8aed89
Manohar008148927 <008148927@coyote.csusb.edu>
2024-09-17 15:29:38 -0700

    local host running after refreshing issue fixed

--- app.py
+++ app.py
@@ -8,12 +8,4 @@ def main():
     st.title("Hello from Team 4")
     
 if __name__ == "__main__":
-    # If not already running in a Streamlit subprocess, start one
-    if not os.getenv('STREAMLIT_RUNNING'):
-        # Set an environment variable to indicate that Streamlit is running
-        os.environ['STREAMLIT_RUNNING'] = '1'
-        # Run the Streamlit app in a new subprocess on port 5004
-        subprocess.run(["streamlit", "run", __file__, "--server.port=5004"])
-    else:
-        # If the environment variable is set, execute the main function
-        main()
+    main()
