52f3a61babab4a5cbd344a86af3bba65a80d08c7
SaiKrishna1565 <008501565@coyote.csusb.edu>
2024-09-23 10:44:32 -0700

    added 10 statistical reports

--- app.py
+++ app.py
@@ -7,11 +7,19 @@ st.title("Team4 Chatbot")
 
 # Sidebar header for static report metrics
 st.sidebar.header("10 Statistics Report")
-
+total_questions = st.sidebar.write("Number of Questions")
+correct_answers = st.sidebar.write("Number of Correct Answers")
+incorrect_answers = st.sidebar.write("Number of Incorrect Answers")
+user_engagement = st.sidebar.write("User Engagement Metrics")
+avg_response_time = st.sidebar.write("Avg Response Time (s)")
+accuracy_rate = st.sidebar.write("Accuracy Rate (%)")
+common_topics = st.sidebar.write("Common Topics or Keywords")
+user_satisfaction = st.sidebar.write("User Satisfaction Ratings (1-5)")
+improvement_over_time = st.sidebar.write("Improvement Over Time")
+feedback_summary = st.sidebar.write("Feedback Summary")
+daily_statistics = st.sidebar.write("Daily Statistics")
 
 # Chat input box for user
 user_input = st.chat_input("Enter your question here")
 
 # Process input if user has entered a message
-if user_input:
-    handle_user_input(user_input)
